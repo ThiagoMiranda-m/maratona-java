f804e002d989f5f3403154dd573fdf05f03d5a84
ThiagoMiranda-m <thiagoanm75@gmail.com>
2025-02-26 21:26:44 -0300

    git commit

--- Maratona DevDojo/src/javacore/javaintrometodos/dominio/Calculator.java
+++ Maratona DevDojo/src/javacore/javaintrometodos/dominio/Calculator.java
@@ -8,6 +8,12 @@ public class Calculator {
     }
 
     public void subtraiDoisNumeros(){
+
         System.out.println(21-2);
     }
+
+    public void multiplicaDoisNumeros(int num1, int num2){
+        System.out.println(num1 * num2);
+    }
+
 }
---
+++ Maratona DevDojo/src/javacore/javaintrometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package javacore.javaintrometodos.test;
+
+import javacore.javaintrometodos.dominio.Calculator;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculator calculadora1 = new Calculator();
+        calculadora1.multiplicaDoisNumeros(10, 20);
+    }
+}
