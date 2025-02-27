8f158a67b25bf86bfaf69793b5e2ba64a87f06aa
ThiagoMiranda-m <thiagoanm75@gmail.com>
2025-02-26 21:48:46 -0300

    git commit

---
+++ Maratona DevDojo/src/javacore/javaintrometodos/test/CalculadoraTest03.java
@@ -0,0 +1,11 @@
+package javacore.javaintrometodos.test;
+
+import javacore.javaintrometodos.dominio.Calculator;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculator calculadora = new Calculator();
+        double result = calculadora.divideDoisNumeros(20, 10);
+        System.out.println(result);
+    }
+}
